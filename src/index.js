3751827793a4c6793e62ff18fa6a253ece88acfd
sksabbirhossain <sksabbirhossain4@gmail.com>
2023-01-31 14:31:17 +0600

    created a redux store

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-    <Toaster position="top-right" />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster position="top-right" />
+    </BrowserRouter>
+  </Provider>
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
+import Card from "../components/Card/Card";
 
 const Home = () => {
-    return (
-        <div>
-            <h3>home pages</h3>
+  const state = useSelector((state) => state)
+  
+  console.log(state)
+  return (
+    <div className="container">
+      <div className="mt-3">
+        <div className="row">
+          <div className="col-sm-6 col-md-4 col-lg-3 ">
+            <Card />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3 ">
+            <Card />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
